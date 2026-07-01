d35a183de21bc5aa7233c4741cbd67c218a53c97
Kaustubh Mishra <143159061+user-kaus2bh@users.noreply.github.com>
2026-07-01 11:20:01 +0530

    Time: 96 ms (89.57%), Space: 15.8 MB (44.03%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def insertionSortList(self, head):
+        if not head or not head.next:
+            return head
+        dummy = ListNode(-float('inf'))
+        dummy.next = head
+        cur = head
+        while cur and cur.next:
+            if cur.next.val >= cur.val:
+                cur = cur.next
+            else:
+                tmp = cur.next
+                cur.next = tmp.next
+                pre = dummy
+                while pre.next.val < tmp.val:
+                    pre = pre.next
+                tmp.next = pre.next
+                pre.next = tmp
+        return dummy.next
