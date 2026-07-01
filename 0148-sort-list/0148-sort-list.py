ade38f55504ec547844f48f3c8daf40e3f8de404
Kaustubh Mishra <143159061+user-kaus2bh@users.noreply.github.com>
2026-07-01 11:20:57 +0530

    Time: 432 ms (47.64%), Space: 50.7 MB (51.88%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+        slow, fast = head, head
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = None
+        left = self.sortList(head)
+        right = self.sortList(slow)
+        return self.merge(left, right)
+
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1: cur.next = l1
+        if l2: cur.next = l2
+        return dummy.next
